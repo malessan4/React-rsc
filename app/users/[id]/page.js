0bc5423b84a8c32dcb909a636e00424952f4613b
Matias-Alessandrello <matialess@gmail.com>
2024-05-24 10:37:26 -0300

    en paralelo, mejor rendimiento, pero todavia se puede usar otra forma mejor

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -2,10 +2,12 @@ import getUserData from '@/app/utils/getUserData';
 import getUserPosts from '@/app/utils/getUserPosts';
 
 export default async function UserPage({params: { id } }) {
-    const data = await getUserData(id);
-    const posts = await getUserPosts(id);
+    const dataP = getUserData(id);
+    const postsP =  getUserPosts(id);
+
+    const [data, posts] = await Promise.all([dataP, postsP]);
     return(
-        <main className="p-30">
+        <main className="p-10">
             <h1>Usuario: </h1>
             <p>{data.name}</p>
             <p>{data.email}</p>
